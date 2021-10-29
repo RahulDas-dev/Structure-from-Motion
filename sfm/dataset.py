c57e657e8c6e65098ba38ee1bec2ea595d70f9df
RahulDas-dev <r.das699@gmail.com>
2021-10-29 10:56:27 +0000

    Another class for Unittesting added for Dataset

--- sfm/dataset.py
+++ sfm/dataset.py
@@ -10,7 +10,11 @@ VALID_EXTENSION: List[str] = ['jpg', 'JPEG', 'png', 'PNG']
 class Dataset(object):
     """Data Set Loader."""
 
-    __slots__ = ('__extension', '__images')
+    __extension: List[str]
+    __images: List[str]
+    __sorted: bool
+
+    __slots__ = ('__extension', '__images', '__sorted')
 
     def __init__(self, image_dir: Union[str, List[str]], extension: Union[str, List[str]] = VALID_EXTENSION):
         if image_dir is None or image_dir == []:
@@ -36,23 +40,31 @@ class Dataset(object):
                 images = list(filter(lambda x: path.basename(x).split('.')[1] in self.__extension, os.listdir(image_dir)))
                 if len(images) == 0:
                     raise FileNotFoundError('image_dir Should Contain Images')  
-                self.__images = images       
+                self.__images = [os.path.join(image_dir, file) for file in images]       
             else:
                 raise NotADirectoryError('image_dir Should Valid Directory')
+        self.__sorted = False        
 
     def __len__(self) -> int:
         return len(self.__images)
 
-    def sortImages(self, sortfunc: Callable[[str], bool]):
-        if not isinstance(sortfunc, Callable[[str], bool]):
-            raise ValueError('Sort Function is not valid')
+    @property
+    def sorted(self):
+        return self.__sorted    
+
+    def sortImages(self, sortfunc: Callable[[str], Union[int, float]]):
         self.__images.sort(key=sortfunc)
+        self.__sorted = True
 
     def __getitem__(self, index: int) -> str:
         return self.__images[index]
 
     def getPairs(self) -> List[str]:
         return combinations(self.__images, 2)
+
+    @property
+    def getImagesList(self) -> List[str] :
+        return self.__images 
 
 
 
--- sfm/test/test_dataset.py
+++ sfm/test/test_dataset.py
@@ -1,9 +1,13 @@
 import unittest
 import os
+from random import shuffle
 
 from sfm.dataset import Dataset, VALID_EXTENSION
 
-class TestDatset(unittest.TestCase):
+imageDir = '/home/common/aravind/ashley/data/cleaned_dataset'
+imageDir1 = '/mnt/sfm/SfM_quality_evaluation/Benchmarking_Camera_Calibration_2008/Herz-Jesus-P25/images'
+
+class TestDatsetInstantiation(unittest.TestCase):
 
     def test_create_dataset_with_none(self):
         with self.assertRaises(ValueError) as context:
@@ -11,32 +15,70 @@ class TestDatset(unittest.TestCase):
         self.assertEqual('image_dir Should Valid Directory or List of Images', str(context.exception))    
 
     def test_create_dataset_invalid_directory(self):
+        invalidDir=os.path.join(imageDir,'invalidDir')
         with self.assertRaises(NotADirectoryError) as context:
-            data = Dataset('/home/common/aravind/ashley/data/cleaned_datase')
+            data = Dataset(invalidDir)
         self.assertEqual('image_dir Should Valid Directory', str(context.exception))
 
     def test_create_dataset_valid_directory_no_image(self):
         with self.assertRaises(FileNotFoundError) as context:
-            data = Dataset('/home/common/aravind/ashley/data/cleaned_dataset')
+            data = Dataset(imageDir)
         self.assertEqual('image_dir Should Contain Images', str(context.exception))  
 
-    def test_create_dataset_invalid_extension(self):
-        img_dir = '/home/common/aravind/ashley/data/cleaned_dataset'
-        images = [ os.path.join(img_dir, file) for file in os.listdir(img_dir)]
+    def test_create_dataset_invalid_file_extension(self):
+        images = [ os.path.join(imageDir, file) for file in os.listdir(imageDir)]
         with self.assertRaises(FileExistsError) as context:
             data = Dataset(images)
         self.assertEqual(f'Files Extension should be {VALID_EXTENSION}', str(context.exception))
 
     def test_create_dataset_with_file_not_exists(self):
-        img_dir = '/home/common/aravind/ashley/data/cleaned_dataset'
-        images = [ os.path.join(img_dir, file) for file in os.listdir(img_dir)]
-        images.append( os.path.join(img_dir,'invalid.jpg'))
+        images = [ os.path.join(imageDir, file) for file in os.listdir(imageDir)]
+        images.append( os.path.join(imageDir,'invalid.jpg'))
         with self.assertRaises(FileNotFoundError) as context:
             data = Dataset(images)
-        self.assertEqual('All images Files should be Existing', str(context.exception))                
+        self.assertEqual('All images Files should be Existing', str(context.exception))     
+
+    def test_create_dataset_with_invalid_extesion(self):
+        with self.assertRaises(ValueError) as context:
+            data = Dataset(imageDir, 'csv')
+        self.assertEqual('extension is not Valid', str(context.exception))   
+
+    def test_create_dataset_with_invalid_extesion_list(self):
+        with self.assertRaises(ValueError) as context:
+            data = Dataset(imageDir, ['csv', 'obj', 'jpg'])
+        self.assertEqual('extension is not Valid', str(context.exception)) 
 
 
+class TestDatsetObject(unittest.TestCase):
 
+    def setUp(self) -> None:
+        super().setUp()
+        self.dataset =  Dataset(imageDir1)
+
+    def teardown(self):
+        self.dataset = None  
+
+    def test_length(self):
+        images = [ file for file in os.listdir(imageDir1) if os.path.basename(file).split('.')[1] in VALID_EXTENSION]
+        self.assertEqual(len(self.dataset), len(images))
+
+    def test_getItem(self):
+        allFilesvalid = all([os.path.exists(self.dataset[i]) for i in range(len(self.dataset))])
+        self.assertEqual(allFilesvalid, True)
+
+    def test_sorted(self):
+        self.assertFalse(self.dataset.sorted)  
+        images = [ file for file in os.listdir(imageDir1) if os.path.basename(file).split('.')[1] in VALID_EXTENSION]
+        images = [ os.path.join(imageDir1,file) for file in images]
+        shuffle((images))
+        sortFunction = lambda x : int(os.path.basename(x).split('.')[0])   
+        images.sort(key=sortFunction)
+        self.dataset.sortImages(sortFunction)
+        self.assertListEqual(images,self.dataset.getImagesList)
+        self.assertTrue(self.dataset.sorted) 
+        
+
+   
 
 
 
