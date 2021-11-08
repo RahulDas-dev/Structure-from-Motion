fa047cc01d97a278f9d6092a0ca1e68b3af28efe
Rahul_Das <r.das699@gmail.com>
2021-11-08 16:53:08 +0530

    exif reader modified

--- sfm/exif/exif_reader.py
+++ sfm/exif/exif_reader.py
@@ -1,7 +1,9 @@
 """Module Reads Exif Data."""
 
 import logging
-from typing import Dict, Tuple
+from typing import Dict
+
+import cv2
 
 from exif import Image
 
@@ -12,21 +14,34 @@ class ExifReader(object):
     """Module Reads Exif Data."""
 
     metadata: Dict
+    img_height: int
+    img_width: int
 
     def __init__(self, image_path: str):
         with open(image_path, "rb") as image_file:
             self.metadata = Image(image_file)
+        image = cv2.imread(image_path)
+        self.img_height, self.img_width = image.shape[:2]
+        image = None
 
     @property
     def has_exif_metadat(self) -> bool:
         return self.metadata.has_exif
 
     @property
-    def dimention(self) -> Tuple[int, int]:
-        return (self.metadata.get("image_height", -1), self.metadata.get("image_width", -1))
+    def exif_version(self) -> str:
+        return self.metadata.exif_version
 
     @property
-    def maker(self) -> bool:
+    def height(self) -> int:
+        return self.metadata.get("image_height", self.img_height)
+
+    @property
+    def width(self) -> int:
+        return self.metadata.get("image_width", self.img_width)
+
+    @property
+    def make(self) -> bool:
         return self.metadata.get("lens_make", None)
 
     @property
@@ -42,12 +57,25 @@ class ExifReader(object):
         pass
 
     @property
-    def timestamp(self) -> bool:
+    def timestamp(self):
         time_stamp = self.metadata.get("gps_timestamp", None)
         date_stamp = self.metadata.get("gps_datestamp", None)
         if time_stamp is not None and date_stamp is not None:
             try:
-                tims_in_sec = self.__format_date_time(date_stamp, time_stamp)
-            except (TypeError, ValueError):    
+                times_in_sec = self.__format_date_time(date_stamp, time_stamp)
+            except (TypeError, ValueError):
+                pass
 
-    def             
+    def data_as_dictonary(self) -> Dict:
+        status = f"contains EXIF (version {self.exif_version}) information."
+        return {
+            "make": self.make,
+            "model": self.model,
+            "width": self.width,
+            "height": self.height,
+            # "projection_type": projection_type,
+            # "focal_ratio": focal_ratio,
+            "orientation": self.orientation,
+            # "capture_time": capture_time,
+            # "gps": geo,
+        }
